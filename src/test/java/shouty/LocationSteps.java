9fde2711ceb65fdb0701064085719de8fb372372
Gaspar Nagy <gaspar.nagy@gmail.com>
2020-04-06 12:11:26 +0200

    P16-another-level-of-indirection: Introduce ShoutyHelper

--- src/test/java/shouty/LocationSteps.java
+++ src/test/java/shouty/LocationSteps.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 public class LocationSteps {
     @Autowired
-    private Shouty shouty;
+    private ShoutyHelper shoutyHelper;
 
     @Given("{word} is at {int}, {int}")
     public void user_is_at(String name, int xCoord, int yCoord) {
-        shouty.setLocation(name, new Coordinate(xCoord, yCoord));
+        shoutyHelper.shouty.setLocation(name, new Coordinate(xCoord, yCoord));
     }
 
     @Given("people are located at")
     public void peopleAreLocatedAt(List<PersonLocation> personLocations) {
 
         for (PersonLocation personLocation: personLocations){
-            shouty.setLocation(personLocation.name,
+            shoutyHelper.shouty.setLocation(personLocation.name,
                     new Coordinate(personLocation.x, personLocation.y));
         }
     }
--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -17,31 +17,31 @@ import static org.junit.Assert.assertEquals;
 public class ShoutSteps {
     private static final String ARBITRARY_MESSAGE = "Hello, world";
     @Autowired
-    private Shouty shouty;
+    private ShoutyHelper shoutyHelper;
 
     @When("{word} shouts")
     public void user_shouts(String name) {
-        shouty.shout(name, ARBITRARY_MESSAGE);
+        shoutyHelper.shouty.shout(name, ARBITRARY_MESSAGE);
     }
 
     @Then("Lucy should not hear Oscar")
     public void lucy_should_not_hear_oscar() {
-        assertFalse(shouty.getShoutsHeardBy("Lucy").containsKey("Oscar"));
+        assertFalse(shoutyHelper.shouty.getShoutsHeardBy("Lucy").containsKey("Oscar"));
     }
 
     @Then("Lucy should hear Sean")
     public void lucy_should_hear_sean() {
-        assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
+        assertEquals(1, shoutyHelper.shouty.getShoutsHeardBy("Lucy").size());
     }
 
     @Then("Lucy should hear nothing")
     public void lucy_should_hear_nothing() {
-        assertEquals(emptyMap(), shouty.getShoutsHeardBy("Lucy"));
+        assertEquals(emptyMap(), shoutyHelper.shouty.getShoutsHeardBy("Lucy"));
     }
 
     @Then("Lucy should hear {int} shouts from Sean")
     public void lucyShouldHearShoutsFromSean(int expectedNumberOfShouts) {
-        Map<String, List<String>> shoutsHeard = shouty.getShoutsHeardBy("Lucy");
+        Map<String, List<String>> shoutsHeard = shoutyHelper.shouty.getShoutsHeardBy("Lucy");
         List<String> shoutsByShouter = shoutsHeard.get("Sean");
         assertEquals(expectedNumberOfShouts, shoutsByShouter.size());
     }
